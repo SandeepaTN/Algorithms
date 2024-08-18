9deda2f1bb18e574a8515dfb99cb16c8a77736e5
sandeepa t n <saandeepatalagini@gmail.com>
2024-08-18 21:11:27 +0530

    slighty improved

--- greedy/moneyChange.py
+++ greedy/moneyChange.py
@@ -1,17 +1,12 @@
 def change(money):
-    n=0
-    while money > 0:
-        if money >= 10:
-            n+= money // 10
-            money = money % 10
-        elif money >= 5:
-            n+= money // 5
-            money = money % 5
-        else:
-            n += money
-            money = 0
+    n=money//10
+    money=money%10
+    n+=money//5
+    money=money%5
+    n+=money
 
-    return n
+
+    return n 
 
 
 if __name__ == '__main__':
